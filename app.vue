53f64bff7376da8b627b70c3d09befd1f04f96bc
axreer <saidaxrorpc7@gmail.com>
2024-07-10 20:29:47 +0500

    changed

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+    useHead({
+        titleTemplate: (titleChunk) => {
+            return titleChunk ? `${titleChunk} - Sobirov Saidaxror - Web CV` : "Sobirov Saidaxror - Web CV";
+        },
+        link: [{ rel: "icon", type: "image/x-icon", href: "/icon.svg" }],
+    });
+    useSeoMeta({
+        ogImage: "/icon.svg",
+    });
+</script>
+
 <template>
-  <div>
-    <NuxtPage/>
-   
-  </div>
+    <div>
+        <NuxtPage />
+    </div>
 </template>
